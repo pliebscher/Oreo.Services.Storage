74bbae583d36325462e61fc86fd342632af2987a
Philip Liebscher <11465292+pliebscher@users.noreply.github.com>
2022-01-31 20:49:15 -0800

    Add project files.

---
+++ Oreo.Services.Storage.Core/IStorageService.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace Oreo.Services.Storage.Core
+{
+    /// <summary>
+    /// Defines basic storge service functions.
+    /// </summary>
+    /// <remarks>
+    /// fileName(s) should include a path relative to the root of the Store. e.g. "some\path\filename.ext"
+    /// </remarks>
+    public interface IStorageService
+    {
+        /// <summary>
+        /// Returns a list of files in the Store.
+        /// </summary>
+        IEnumerable<string> List();
+
+        /// <summary>
+        /// Loads a file from the Store.
+        /// </summary>
+        /// <param name="fileName"></param>
+        MemoryStream Load(string fileName);
+
+        /// <summary>
+        /// Saves a file to the Store.
+        /// </summary>
+        /// <param name="fileData"></param>
+        /// <param name="fileName"></param>
+        void Save(MemoryStream fileData, string fileName);
+
+        /// <summary>
+        /// Checks if a file exists in the Store.
+        /// </summary>
+        /// <param name="fileName"></param>
+        bool Exists(string fileName);
+
+        /// <summary>
+        /// Deletes a file from the Store.
+        /// </summary>
+        /// <param name="fileName"></param>
+        void Delete(string fileName);
+    }
+}
---
+++ Oreo.Services.Storage.Core/StorageServiceException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+
+namespace Oreo.Services.Storage.Core
+{
+    [Serializable()]
+    public class StorageServiceException : Exception
+    {
+        public StorageServiceException()
+        {
+        }
+
+        public StorageServiceException(string message) : base(message)
+        {
+        }
+
+        public StorageServiceException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected StorageServiceException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+        }
+    }
+}
